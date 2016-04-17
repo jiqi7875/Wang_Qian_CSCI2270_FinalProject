42e78777c3bae7d269d59136acb3a4bbdedbcf76
jiqi7875 <jiqi7875@colorado.edu>
2016-04-17 00:07:55 -0600

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,5 +1,1 @@
-# Wang_Qian_CSCI2270_FinalProject
 
-Our project is about a system of renting books. We are planning to use BST data
-structure mainly to build the system, and then use other data structures to
-show other useful information.
